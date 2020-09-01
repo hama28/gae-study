8f15c65f3c6f5c31e053fab9a2390758ced52176
hata_poly <mh.mundus@gmail.com>
2020-09-01 14:09:09 +0900

    9/1 GETメソッド実習

--- guestbook/main.py
+++ guestbook/main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, abort
+from flask import Flask, render_template, abort, request
+
 
 app = Flask(__name__)
 
@@ -8,6 +9,32 @@ def home():
     message = 'App Engine勉強会 にようこそ'
     return render_template('index.html', message=message)
 
+
+@app.route('/api/greetings')
+def greetings(key_id=None):
+    if request.method == 'GET':
+        igarashi = {
+            'id': 1,
+            'author': 'Tsuyoshi Igarashi',
+            'message': 'Hello'
+        }
+        miyayama = {
+            'id': 2,
+            'author': 'Ryutaro Miyayama',
+            'message': 'Looks good to me'
+        }
+        shirakawa = {
+            'id': 3,
+            'author': 'Mai Shirakawa',
+            'message': 'Banana!'
+        }
+        greetings = [igarashi, miyayama, shirakawa]
+        res = {
+            'greetings': greetings
+        }
+        return res
+
+
 @app.route('/err500')
 def err500():
     abort(500)
@@ -16,6 +43,7 @@ def err500():
 @app.errorhandler(404)
 def error_404(exception):
     return {'message': 'Error: Resouce not found.'}, 404
+
 
 @app.errorhandler(500)
 def error_500(exception):
